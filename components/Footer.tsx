eeba247633284454311eb3e06c3fa81d703f4931
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2024-11-19 23:29:55 +0400

    add structure

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='container'>
+      Some Company 2024
+    </footer>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='container'>
+      <p>Some Company </p>
+      <button>Contact us</button>
+    </header>
+  )
+}
